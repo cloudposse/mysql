691a922758a9927faeb1b7cb43d3c85391f461dd
Erik Osterman <e@osterman.com>
2015-12-22 03:10:13 -0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM mysql:latest
+MAINTAINER  Erik Osterman "e@osterman.com"
+
+# System ENV
+ENV TIMEZONE Etc/UTC
+ENV DEBIAN_FRONTEND noninteractive
+ENV PATH "$PATH:/usr/local/bin"
+ENV TERM xterm
+ENV PERL_MM_USE_DEFAULT true
+
+RUN apt-get update && \
+    apt-get -y install procps && \
+    apt-get -y install libdbd-mysql libdbd-mysql-perl && \
+    rm -f /var/log/mysql/error.log && \
+    ln -s /dev/stderr /var/log/mysql/error.log
+
+RUN chown -R mysql:mysql /var/lib/mysql/
+ADD entrypoint.sh /entrypoint.sh
+ADD my.cnf /etc/mysql/conf.d/
+CMD mysqld_safe
---
+++ entrypoint.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+tempSqlFile='/tmp/mysql-first-time.sql'
+
+rm -f $tempSqlFile
+echo "ENTRYPOINT: $@"
+if [ "${1:0:1}" = '-' ]; then
+	set -- mysqld "$@"
+fi
+
+if [ "$1" = 'mysqld_safe' ] || [ "$1" = 'mysqld' ]; then
+	# read DATADIR from the MySQL config
+	DATADIR="$(mysqld --verbose --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
+	
+	if [ ! -d "$DATADIR/mysql" ]; then
+		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" ]; then
+			echo >&2 'error: database is uninitialized and MYSQL_ROOT_PASSWORD not set'
+			echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
+			exit 1
+		fi
+		
+		echo 'Running mysql_install_db ...'
+		mysql_install_db --datadir="$DATADIR"
+		echo 'Finished mysql_install_db'
+		
+		# These statements _must_ be on individual lines, and _must_ end with
+		# semicolons (no line breaks or comments are permitted).
+		# TODO proper SQL escaping on ALL the things D:
+		
+		tempSqlFile='/tmp/mysql-first-time.sql'
+
+    # Truncate file
+    :> "$tempSqlFile"
+
+    # Basics
+    echo "DELETE FROM mysql.user;" >> "$tempSqlFile"
+    echo "CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> "$tempSqlFile"
+    echo "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;" >> "$tempSqlFile"
+    echo "DROP DATABASE IF EXISTS test;" >> "$tempSqlFile"
+
+		if [ -n "$MYSQL_DATABASE" ]; then
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
+		fi
+		
+		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
+			echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> "$tempSqlFile"
+			
+			if [ "$MYSQL_DATABASE" ]; then
+				echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%' ;" >> "$tempSqlFile"
+			fi
+		fi
+		
+		echo 'FLUSH PRIVILEGES ;' >> "$tempSqlFile"
+
+		set -- "$@" --init-file="$tempSqlFile"
+		
+    if [ -f "$DATADIR/mysqldump.sql" ]; then
+      # Start the database first in the background
+      "$@" --skip-networking &
+      mysql_pid=$!
+      echo -n "Starting mysqld"
+      until mysqladmin -u"root" -p"$MYSQL_ROOT_PASSWORD" ping &>/dev/null; do
+        echo -n "."; sleep 0.2
+      done
+      echo
+      echo "Populating db $MYSQL_DATABASE from $DATADIR/mysqldump.sql"
+      time mysql -u"root" -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < "$DATADIR/mysqldump.sql"
+      # Shut the database back down
+      time mysqladmin -u"root" -p"$MYSQL_ROOT_PASSWORD" shutdown
+      wait $mysql_pid
+    fi
+
+    # truncate init file so we don't leave passwords laying around
+    #:> $tempSqlFile
+
+	fi
+	
+	chown -R mysql:mysql "$DATADIR"
+
+fi
+echo "Starting mysql with '$@'"
+exec "$@"
